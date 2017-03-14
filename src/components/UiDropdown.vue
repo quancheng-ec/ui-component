e05c733c638f440d88d7347eff885e1a42812bb2
zsy <zsy@zsydeMacBook-Pro.local>
2017-03-14 15:38:41 +0800

    提交文件补充

--- src/components/UiDropdown.vue
+++ src/components/UiDropdown.vue
@@ -1,64 +1,96 @@
 <template>
-  <label class="checkbox ui-checkbox_label" :class="checkboxClass">
-    <input type="checkbox" :name="name" :disabled="disabled" :value="inputValue" @change="updateValue($event.target)" :checked="value">
-    <label></label>
-    <slot>{{label}}</slot>
-  </label>
+  <div class="btn-group m-r-10 " :class="boxClass" @click="openDropdown">
+    <button aria-expanded="false" data-toggle="dropdown" class="btn dropdown-toggle waves-effect waves-light" :class="btnClass" type="button">
+      <slot>{{label}}</slot> <span class="caret"></span>
+    </button>
+    <slot name="menu">
+      <ul role="menu" class="dropdown-menu">
+        <template v-for="item in list">
+          <li><a @click="clickItem(item, $event)" href="javascript:void(0)">{{item[textKey]}}</a></li>
+          <li v-if="item.divider" class="divider"></li>
+        </template>
+      </ul>
+    </slot>
+
+  </div>
 </template>
 
 <script>
+  import EventListener from '../libs/EventListener.es6'
   import classNames from 'classnames'
   export default {
+    props : {
+      label : {
+        type: String,
+        default: 'dropdown'
+      },
+      textKey : {
+        type: String,
+        default: 'text'
+      },
+      list: {
+        type: Array,
+        default: []
+      },
+      dropup: {
+        type: Boolean,
+        default: false
+      },
+      type: {
+        type : [String, Array],
+        default : 'info'
+      },
+      clickClose: {
+        type: Boolean,
+        default: true
+      }
+
+    },
     data(){
       return {
+        open : false
       }
-    },
-    props: {
-      type: {
-        type: [String, Array]
-      },
-      value: {
-      },
-      label: {
-        type : String,
-        default : ''
-      },
-      inputValue: {
-        type: String,
-        default : ''
-      },
-      disabled:{
-        type : Boolean,
-        default : false
-      },
-      name: {
-        type: String,
-        default : ''
-      }
-    },
-    created(){
     },
     computed: {
-      checkboxClass() {
+      boxClass(){
+        return {
+          open : this.open,
+          dropup : this.dropup
+        }
+      },
+      btnClass() {
         const Classes = this.type?(typeof this.type === 'string' ? this.type.split(' ') : this.type):[];
-        return classNames(Classes.map(cls => 'checkbox-' + cls));
+        return classNames(Classes.map(cls => 'btn-' + cls));
       }
     },
     methods: {
-      updateValue(target){
-        this.$emit('input', target.checked)
+      openDropdown(){
+        this.open = !this.open;
+      },
+      clickItem(item, e){
+        this.$emit('no-click', item, e);
       }
+    },
+    mounted() {
+      const el = this.$el;
+      this._closeEvent = EventListener.listen(window, 'click', (e)=> {
+        if(this.clickClose){
+          if (!el.contains(e.target)){
+            this.open = false;
+          }
+        }else{
+          if(el.contains(e.target) && e.target.nodeName.toLowerCase() == 'button' && !this.open){
+            this.open = false;
+            return;
+          }
+          this.open = el.contains(e.target);
+        }
+      });
+    },
+    beforeDestroy() {
+      if (this._closeEvent) this._closeEvent.remove();
     }
   }
 </script>
 <style lang='stylus' rel='stylesheet/stylus'>
-  .ui-checkbox_label
-    display inline-flex
-    align-items center
-    cursor pointer
-    & input[type=checkbox]
-      zoom 150%
-      margin 0
-      left 0
-      top 0
 </style>
