56d7110ff0e382010d6dc7fe5c1f8c7f7d99250d
zsy <zsy@zsydeMacBook-Pro.local>
2017-03-31 11:28:28 +0800

    修改dropdown默认样式

--- src/components/UiDropdown.vue
+++ src/components/UiDropdown.vue
@@ -1,17 +1,24 @@
 <template>
-  <div class="btn-group m-r-10 " :class="boxClass" @click="openDropdown">
-    <button aria-expanded="false" data-toggle="dropdown" class="btn dropdown-toggle waves-effect waves-light" :class="btnClass" type="button">
+  <div class="btn-group m-r-10 "
+       :class="boxClass"
+       @click="openDropdown">
+    <button aria-expanded="false"
+            data-toggle="dropdown"
+            class="btn dropdown-toggle waves-effect waves-light"
+            :class="btnClass"
+            type="button">
       <slot>{{_showLabel()}}</slot> <span class="caret"></span>
     </button>
     <slot name="menu">
-      <ul role="menu" class="dropdown-menu" >
+      <ul role="menu"
+          class="dropdown-menu">
         <template v-for="item in list">
           <li :class="{'dropdown-disabled':item.disabled}">
-            <a @click="clickItem(item, $event)"  href="javascript:void(0)">{{typeof item == 'object'?item[textKey]:item}}
-              <i v-if="select && _isChecked(item)" class="dropdown_icon" :class="iconClass"></i>
-            </a>
+            <a @click="clickItem(item, $event)"
+               href="javascript:void(0)">{{typeof item == 'object'?item[textKey]:item}}<i v-if="select && _isChecked(item)" class="dropdown_icon" :class="iconClass"></i></a>
           </li>
-          <li v-if="item.divider" class="divider"></li>
+          <li v-if="item.divider"
+              class="divider"></li>
         </template>
       </ul>
     </slot>
@@ -23,42 +30,42 @@
   import EventListener from '../libs/EventListener.es6'
   import classNames from 'classnames'
   export default {
-    props : {
-      value: {},//支持v-model，只有在props激活select属性生效
+    props: {
+      value: {},  //  支持v-model，只有在props激活select属性生效
       label: {
         type: String,
         default: ''
       },
-      maxLabel: { //label最大显示字数
+      maxLabel: { //  label最大显示字数
         type: Number
       },
-      textKey : {//设置下拉菜单显示的文本值，list必须为对象数组
+      textKey: {//  设置下拉菜单显示的文本值，list必须为对象数组
         type: String,
         default: 'text'
       },
-      valueKey : {//设置下拉菜单用于的ID值，用于v-model双绑实现选中效果，list必须为对象数组
+      valueKey: {// 设置下拉菜单用于的ID值，用于v-model双绑实现选中效果，list必须为对象数组
         type: String,
         default: 'value'
       },
       list: {//渲染下拉菜单数组
         type: Array,
-        default(){
-          return [];
+        default() {
+          return []
         }
       },
-      dropup: {//改变下拉菜单展开方向，是否向上展开
+      dropup: {// 改变下拉菜单展开方向，是否向上展开
         type: Boolean,
         default: false
       },
-      type: {//设置dropdown样式(与button样式相同)
-        type : [String, Array],
-        default : 'info'
+      type: {// 设置dropdown样式(与button样式相同)
+        type: [String, Array],
+        default: 'default outline'
       },
-      clickClose: {//选中下拉项后是否关闭下拉菜单
+      clickClose: {// 选中下拉项后是否关闭下拉菜单
         type: Boolean,
         default: true
       },
-      select: {//将dropdown改变为select形式的组件，支持v-model绑定
+      select: {// 将dropdown改变为select形式的组件，支持v-model绑定
         type: Boolean,
         default: false
       },
@@ -66,11 +73,11 @@
         type: Boolean,
         default: false
       },
-      icon: {//设置选中状态icon，只有在select形态下有效
+      icon: {// 设置选中状态icon，只有在select形态下有效
         type: String,
         default: 'check'
       },
-      placeHolder: {//默认显示文本，只有在select形态下有效
+      placeHolder: {//  默认显示文本，只有在select形态下有效
         type: String
       },
       returnObject: {
@@ -78,115 +85,115 @@
         default: false
       }
     },
-    data(){
+    data() {
       return {
-        open : false
+        open: false
       }
     },
     computed: {
-      boxClass(){
+      boxClass() {
         return {
-          open : this.open,
-          dropup : this.dropup
+          open: this.open,
+          dropup: this.dropup
         }
       },
       btnClass() {
-        const Classes = this.type?(typeof this.type === 'string' ? this.type.split(' ') : this.type):[];
-        return classNames(Classes.map(cls => 'btn-' + cls));
+        const Classes = this.type ? (typeof this.type === 'string' ? this.type.split(' ') : this.type) : []
+        return classNames(Classes.map(cls => 'btn-' + cls))
       },
       iconClass() {
         return classNames('fa', 'fa-' + this.icon)
       }
     },
     methods: {
-      openDropdown(){
-        this.open = !this.open;
+      openDropdown() {
+        this.open = !this.open
+        this.$emit('dropdown:opened', this.open)
       },
-      clickItem(item, e){
-        if(item.disabled) return;
-        if(this.select){
-          let value = JSON.parse(JSON.stringify(this.value)) || [];
-          if(this.multiple){
-            let i = value.findIndex(v=>{
-              return (this.returnObject?v[this.valueKey]:v)== item[this.valueKey];
-            });
-            if(i == -1){
-              value.push(this.returnObject?item:item[this.valueKey]);
-            }else{
-              value.splice(i, 1);
+      clickItem(item, e) {
+        if (item.disabled) return
+        if (this.select) {
+          let value = JSON.parse(JSON.stringify(this.value)) || []
+          if (this.multiple) {
+            let i = value.findIndex(v => {
+              return (this.returnObject ? v[this.valueKey] : v) == item[this.valueKey]
+            })
+            if (i == -1) {
+              value.push(this.returnObject ? item : item[this.valueKey])
+            } else {
+              value.splice(i, 1)
             }
-          }else{
-            value = this.returnObject?item:item[this.valueKey];
+          } else {
+            value = this.returnObject ? item : item[this.valueKey]
           }
-          this.$emit('input', value , e);
+          this.$emit('input', value, e)
         }
-        this.$emit('no-click', item, e);
+        this.$emit('no-click', item, e)
       },
-      _value(item){
-        return this.returnObject?item:item[this.valueKey];
+      _value(item) {
+        return this.returnObject ? item : item[this.valueKey]
       },
-      _isChecked(item){
-        if(!this.value) return;
-        let flag = false;
-        if(this.multiple){
-          for(let v of this.value){
-            if((this.returnObject?v[this.valueKey]:v) == item[this.valueKey]){
-              flag = true;
+      _isChecked(item) {
+        if (!this.value) return
+        let flag = false
+        if (this.multiple) {
+          for (let v of this.value) {
+            if ((this.returnObject ? v[this.valueKey] : v) == item[this.valueKey]) {
+              flag = true
               break
             }
           }
-        }else{
-          flag = ((this.returnObject?this.value[this.valueKey]:this.value) == item[this.valueKey]);
+        } else {
+          flag = ((this.returnObject ? this.value[this.valueKey] : this.value) == item[this.valueKey])
         }
-        return flag;
+        return flag
       },
-      _showLabel(){
-        if(this.select){
-//          if(!this.value) return this.placeHolder;
-          let name = [];
-          if(this.multiple){
-            for(let v of this.value){
-              for(let item of this.list){
-                if(item[this.valueKey] == (this.returnObject?v[this.valueKey]:v)){
-                  name.push(item[this.textKey]);
-                  break;
+      _showLabel() {
+        if (this.select) {
+          let name = []
+          if (this.multiple) {
+            for (let v of this.value) {
+              for (let item of this.list) {
+                if (item[this.valueKey] == (this.returnObject ? v[this.valueKey] : v)) {
+                  name.push(item[this.textKey])
+                  break
                 }
               }
             }
-          }else{
-            for(let item of this.list){
-              if(item[this.valueKey] == (this.returnObject?this.value[this.valueKey]:this.value)){
-                name.push(item[this.textKey]);
-                break;
+          } else {
+            for (let item of this.list) {
+              if (item[this.valueKey] == (this.returnObject ? this.value[this.valueKey] : this.value)) {
+                name.push(item[this.textKey])
+                break
               }
             }
           }
-          if(name.length){
-            return (this.maxLabel&&name.join(',').length > this.maxLabel)?`${name.join(',').substr(0, this.maxLabel)}...`:name.join(',');
+          if (name.length) {
+            return (this.maxLabel && name.join(',').length > this.maxLabel) ? `${name.join(',').substr(0, this.maxLabel)}...` : name.join(',')
           }
-          return this.placeHolder;
+          return this.placeHolder
         }
-        return this.maxLabel?this.label:'';
+        return this.maxLabel ? this.label : ''
       }
     },
     mounted() {
-      const el = this.$el;
-      this._closeEvent = EventListener.listen(window, 'click', (e)=> {
-        if(this.clickClose){
-          if (!el.contains(e.target)){
-            this.open = false;
+      const el = this.$el
+      this._closeEvent = EventListener.listen(window, 'click', (e) => {
+        if (this.clickClose) {
+          if (!el.contains(e.target)) {
+            this.open = false
           }
-        }else{
-          if(el.contains(e.target) && e.target.nodeName.toLowerCase() == 'button' && !this.open){
-            this.open = false;
-            return;
+        } else {
+          if (el.contains(e.target) && e.target.nodeName.toLowerCase() == 'button' && !this.open) {
+            this.open = false
+            return
           }
-          this.open = el.contains(e.target);
+          this.open = el.contains(e.target)
         }
-      });
+      })
     },
     beforeDestroy() {
-      if (this._closeEvent) this._closeEvent.remove();
+      if (this._closeEvent) this._closeEvent.remove()
     }
   }
 </script>
