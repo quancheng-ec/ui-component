af7f4b8262aaa8ebb1db5ef295304f47fd0a8c8d
wenjun <wen911119@gmail.com>
2017-05-24 14:28:54 +0800

    UiGridItem的space提供默认值，防止报错

--- src/components/UiGridItem.vue
+++ src/components/UiGridItem.vue
@@ -1,25 +1,25 @@
 <template>
-    <div :class="itemClass">
-        <slot></slot>
-    </div>
+  <div :class="itemClass">
+    <slot></slot>
+  </div>
 </template>
 
 <script>
-  export default {
-    props: {
-      space: {
-        type: [String, Number],
-        required: true
-      },
-      offset: {
-        type: [String, Number],
-        default: 0
-      }
+export default {
+  props: {
+    space: {
+      type: [String, Number],
+      default: 12
     },
-    computed: {
-      itemClass(){
-        return 'col-sm-' + this.space
-      }
+    offset: {
+      type: [String, Number],
+      default: 0
+    }
+  },
+  computed: {
+    itemClass() {
+      return 'col-sm-' + this.space
     }
   }
+}
 </script>
